5b24c3a7577190dc9d7118f834fb90d1078611ec
hasmiksarhatyan <hasmiks88.hs@gmail.com>
2022-05-15 01:03:35 +0400

    figurepainter

--- src/homework/FigurePainter2.java
+++ src/homework/FigurePainter2.java
@@ -32,6 +32,25 @@ public class FigurePainter2 {
             System.out.println();
 
         }
+        for (int i = 0; i < 8; i++) {
+            if (i < 4) {
+                for (int j = 3; j > i; j--) {
+                    System.out.print(" ");
+                }
+                for (int k = 0; k <= i; k++) {
+                    System.out.print(" *");
+                }
+            }
+            if (i > 3) {
+                for (int j = 2; j < i; j++) {
+                    System.out.print(" ");
+                }
+                for (int j = 7; j > i; j--) {
+                    System.out.print("* ");
+                }
+            }
+            System.out.println();
+        }
 
     }
 
