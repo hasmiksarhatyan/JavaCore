c506e23ea6a183232c7a5a40bd8f6285856b8026
hasmiksarhatyan <hasmiks88.hs@gmail.com>
2022-05-19 21:59:29 +0400

    array

---
+++ src/homework/arrayutil/CharArrayExample.java
@@ -0,0 +1,49 @@
+package homework.arrayutil;
+
+public class CharArrayExample {
+    public static void main(String[] args) {
+        char[] chars = {'j', 'a', 'v', 'a', 'l', 'o', 'v', 'e' };
+        char c = 'o';
+        int countofc = 0;
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] == c) {
+                countofc++;
+            }
+        }
+        System.out.println("countofc " + countofc);
+        int h = chars.length / 2 - 1;
+        int k = chars.length / 2;
+        System.out.println("մեջտեղի էլեմենտները");
+        System.out.print(chars[h] + " " + chars[k]);
+        System.out.println();
+        char[] chars3 = {'j', 'a', 'v', 'a', 'l', 'o', 'l', 'y' };
+        int charslastelement = chars3.length - 1;
+        int charssecondlastelement = charslastelement - 1;
+        if (chars3[charslastelement] == 'y' && chars3[charssecondlastelement] == 'l') {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+        char[] bobArray = {'b', 'a', 'b', 'o', 'l', 'a' };
+        for (int i = 0; i < bobArray.length; i++) {
+            if (bobArray[i] == 'b' && bobArray[i + 2] == 'b')
+                System.out.println(true);
+            else
+                System.out.println(false);
+        }
+        char[] text = {' ', ' ', 'b', 'a', 'r', 'e', 'v', ' ', ' ' };
+        for (int j = 0; j < text.length; j++) {
+            if (text[j] != ' ') {
+                System.out.print(text[j]);
+            }
+
+        }
+
+    }
+}
+
+
+
+
+
+
--- src/homework/arrayutil/arrayutil.java
+++ src/homework/arrayutil/arrayutil.java
@@ -1,28 +1,28 @@
-package homework;
+package homework.arrayutil;
 
 public class arrayutil {
     public static void main(String[] args) {
         int[] array = {20, 7, -1, 10, 32, 10, 11, 76, 102, 33};
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < array.length; i++) {
             System.out.print( array[i]+ " " );
         }
         System.out.println();
         int max = array[0];
-        for (int i = 0; i < 10; i++) {
+        for (int i = 1; i < array.length; i++) {
             if (array[i] > max) {
                 max = array[i];
             }
         }
         System.out.println("ամենամեծ էլեմենտը " + max);
         int min = array[0];
-        for (int i = 0; i < 10; i++) {
+        for (int i = 1; i < array.length; i++) {
             if (array[i] < min) {
                 min = array[i];
             }
         }
         System.out.println("ամենափոքր էլեմենտը " + min);
         int oddnums = array[0];
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < array.length; i++) {
             if (array[i] % 2 != 0) {
                 oddnums = array[i];
                 System.out.print( oddnums + " ");
@@ -30,34 +30,33 @@ public class arrayutil {
         }
         System.out.println();
         int evennums = array[0];
-        for (int i = 0; i < 10; i++) {
-            if (array[i] % 2 == 0) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i]!=0 && array[i] % 2 == 0) {
                 evennums = array[i];
                 System.out.print( evennums + " ");
             }
         }
         System.out.println();
         int n = 0;
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < array.length; i++) {
             if (array[i] % 2 == 0) {
-                n = n + 1;
+                n++;
             }
         }
         System.out.println("զույգ էլեմենտների քանակը " + n + " ");
         int m = 0;
         for (int i = 0; i < 10; i++) {
             if (array[i] % 2 != 0) {
-                m = m + 1;
+                m++;
             }
         }
         System.out.println("կենտ էլեմենտների քանակը " + m + " ");
-        double result = 0;
-        for (int i = 0; i < 10; i++) {
-            result = result + array[i];
+        double sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
         }
-        System.out.println("էլեմենտների գումարը " + result);
-        System.out.println("միջին թվաբանականը " + result / array.length);
-
+        System.out.println("էլեմենտների գումարը " + sum);
+        System.out.println("միջին թվաբանականը " + sum / array.length);
 
     }
 }
