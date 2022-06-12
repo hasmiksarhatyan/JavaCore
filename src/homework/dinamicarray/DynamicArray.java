d01aea473df1a01c5841d1d2e369a47a0f56c24d
hasmiksarhatyan <105449028+hasmiksarhatyan@users.noreply.github.com>
2022-06-13 02:48:59 +0400

    Update DynamicArray.java

--- src/homework/dinamicarray/DynamicArray.java
+++ src/homework/dinamicarray/DynamicArray.java
@@ -17,20 +17,16 @@ public class DynamicArray {
             System.out.print(array[i] + " ");
         }
     }
-    //Գրել մեթոդ isEmpty անունով, որը կվերադարձնի true եթե մեր dynamicArray-ի մեջ չունենանք ոչ մի էլեմենտ. Եթե ունենք՝ false
 
     public boolean isEmpty() {
         return size == -1;
     }
 
-    //Գրել մեթոդ getByIndex անունով, որը կընդունի ինդեքս, և կվերադարձնի այդ ինդեքսի տակ ընկած թիվը, եթե չկա թող վերադարձնի 0;
     public int getByIndex(int index) {
         if (index <= size && index > 0) {
             return array[index];
         } else return 0;
-    }
-    //Գրել մեթոդ getFirstIndexByValue, որը կընդունի int value, և եթե մեր մասիվի մեջ ունենք այդ թվից, կվերադարձնի իր ինդեքսը. եթե շատ ունենք, պետք է վերադարձնել առաջինի ինդեքսը
-
+    } 
 
     public int getFirstIndexByValue(int value) {
         for (int i = 0; i <= size; i++) {
@@ -41,14 +37,12 @@ public class DynamicArray {
         return 0;
     }
 
-    //Գրել մեթոդ set անունով, որը կընդունի int index, int value և վալյուն կդնի տրված ինդեքսի տեղը(կփոխարինի էղած արժեքին).
     public void set(int index, int value) {
         if (index <= size && index >= 0) {
             array[index] = value;
             printArray();
         }
     }
-    //Գրել մեթոդ add(int index, int value) որը տրված վելյուն կդնի տրված ինդեքսի տեղը, իսկ էղած թիվը ու կողքի բոլոր թվերը կտանի աջ, ոչ մի թիվ չի կորի
 
     public void add(int index, int value) {
         if (index <= size && index >= 0) {
@@ -64,7 +58,6 @@ public class DynamicArray {
         printArray();
     }
 
-    //Գրել մեթոդ delete(int index) որ տանք ինդեքսը, այդ ինդեքսի տակ գտնվող թիվը հեռացնի մասիվից. (նոր մասիվ պետք չէ սարքել)
     public void delete(int index) {
         if (index <= size && index >= 0) {
             for (int i = index; i <= size; i++) {
